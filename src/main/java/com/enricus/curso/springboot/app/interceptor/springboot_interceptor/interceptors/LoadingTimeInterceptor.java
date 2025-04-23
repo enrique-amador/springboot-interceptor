5462347c76d282fe44bca6401ed84d318a258244
kikux-ubuntu <i.am.kike@gmail.com>
2025-04-23 16:58:50 -0600

    computing delay for a request

--- src/main/java/com/enricus/curso/springboot/app/interceptor/springboot_interceptor/interceptors/LoadingTimeInterceptor.java
+++ src/main/java/com/enricus/curso/springboot/app/interceptor/springboot_interceptor/interceptors/LoadingTimeInterceptor.java
@@ -1,5 +1,7 @@
 package com.enricus.curso.springboot.app.interceptor.springboot_interceptor.interceptors;
 
+import java.util.Random;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.lang.Nullable;
@@ -15,20 +17,30 @@ import jakarta.servlet.http.HttpServletResponse;
 public class LoadingTimeInterceptor implements HandlerInterceptor{
 
     private static final Logger logger = LoggerFactory.getLogger(LoadingTimeInterceptor.class);
+        
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+        throws Exception {
+            HandlerMethod controller = (HandlerMethod)handler;
+            logger.info("preHandle() entrando... " + controller.getMethod().getName());
+
+            long start = System.currentTimeMillis();
+            request.setAttribute("start", start);
+            Random r = new Random();
+            int delay = r.nextInt(500);
+            Thread.sleep(delay);
+            return true;
+    }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
             @Nullable ModelAndView modelAndView) throws Exception {
                 HandlerMethod controller = (HandlerMethod)handler;
-                logger.info("postHandle() " + controller.getMethod().getName());
-    }
-            
-    @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
-            throws Exception {
-                HandlerMethod controller = (HandlerMethod)handler;
-                logger.info("preHandle() " + controller.getMethod().getName());
-                return true;
+                long end = System.currentTimeMillis();
+                long start = (long)request.getAttribute("start");
+                long lapsed = end - start;
+                logger.info("lapsed time: " + lapsed + " milliseconds");
+                logger.info("postHandle() saliendo..." + controller.getMethod().getName());
     }
 
 }
