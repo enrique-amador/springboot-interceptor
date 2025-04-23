3111aae3e6f45befba1775bce50a691688a80e14
kikux-ubuntu <i.am.kike@gmail.com>
2025-04-22 18:27:13 -0600

    first commit

---
+++ src/main/java/com/enricus/curso/springboot/app/interceptor/springboot_interceptor/interceptors/LoadingTimeInterceptor.java
@@ -0,0 +1,25 @@
+package com.enricus.curso.springboot.app.interceptor.springboot_interceptor.interceptors;
+
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class LoadingTimeInterceptor implements HandlerInterceptor{
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+            @Nullable ModelAndView modelAndView) throws Exception {
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+            return true;
+    }
+
+}
